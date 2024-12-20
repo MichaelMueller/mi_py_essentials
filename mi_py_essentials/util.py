b6c71848d102c88f42a74661a84598bbd27bbdd5
Michael Mueller <mueller@mbits.info>
2024-12-20 21:27:54 +0100

    Checkpoint commit as of 2024-12-20-21-27-54.

--- mi_py_essentials/util.py
+++ mi_py_essentials/util.py
@@ -8,9 +8,9 @@ class Util:
             content = await file.read()
             return json.loads(content)
 
-    async def write_json(file_path, data):
+    async def write_json(file_path, data, indent=2):
         """Writes data to a JSON file asynchronously."""
         async with aiofiles.open(file_path, mode='w') as file:
-            content = json.dumps(data, indent=4)
+            content = json.dumps(data, indent=indent)
             logging.getLogger(__name__).debug(f'Writing json to file {file_path}')
             await file.write(content)
